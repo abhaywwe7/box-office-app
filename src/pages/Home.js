5d16a62792497c9e78fd01a713705abf3817b160
abhaywwe7 <abhaywwe7@gmail.com>
2021-06-24 19:53:01 +0530

    showing results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import { apiGet } from "../misc/config";
 const Home = () => {
-  return <MainPageLayout>This is home page dude.</MainPageLayout>;
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState("");
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+      setResults(result);
+      // console.log(result);
+    });
+  };
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSearch();
+    }
+  };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
 };
 
 export default Home;
