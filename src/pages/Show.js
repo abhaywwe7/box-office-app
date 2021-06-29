505b00a52bf64189ebb4103c1b171ab9daafa12d
abhaywwe7 <abhaywwe7@gmail.com>
2021-06-29 12:48:28 +0530

    added reducer and show styling

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,26 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import { useParams } from "react-router";
 import { apiGet } from "../misc/config";
+import ShowMainData from "../components/show/ShowMainData";
+import Details from "../components/show/Details";
+import Seasons from "../components/show/Seasons";
+import Cast from "../components/show/Cast";
+import { ShowPageWrapper, InfoBlock } from "./Show.styled";
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case "FETCH_SUCCESS": {
+      return { isLoading: false, error: null, show: action.show };
+    }
+    case "FETCH_FAILED": {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const [isLoading, setisLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  // const [show, setShow] = useState(null);
+  // const [isLoading, setisLoading] = useState(true);
+  // const [error, setError] = useState(null);
   useEffect(() => {
     let isMounted = true;
     apiGet(`/show/${id}?embed[]=seasons&embed[]=cast`)
       .then((results) => {
         setTimeout(() => {
           if (isMounted) {
-            setShow(results);
-            setisLoading(false);
+            dispatch({ type: "FETCH_SUCCESS", show: results });
+
+            // setShow(results);
+            // setisLoading(false);
           }
         }, 200);
       })
       .catch((err) => {
         if (isMounted) {
-          setError(err.message);
-          setisLoading(false);
+          dispatch({ type: "FETCH_FAILED", error: err.message });
+
+          // setError(err.message);
+          // setisLoading(false);
         }
       });
     return () => {
@@ -35,7 +69,33 @@ const Show = () => {
     return <div>Error occured: {error}</div>;
   }
 
-  return <div>This is show page.</div>;
+  return (
+    <ShowPageWrapper>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.tags}
+      />
+      <InfoBlock>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </InfoBlock>
+      <InfoBlock>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </InfoBlock>
+      <InfoBlock>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </InfoBlock>
+    </ShowPageWrapper>
+  );
 };
 
 export default Show;
